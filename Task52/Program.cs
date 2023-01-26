18e09739d12e4b8383d80e967a096ba608174ace
Artem <grekartemvl@mail.ru>
2023-01-26 15:21:05 +0500

    Add Task52

---
+++ Task52/Program.cs
@@ -0,0 +1,80 @@
+﻿/* 52. Задайте двумерный массив из целых чисел. Найдите
+среднее арифметическое элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.  */
+int GetDataFromUser(string message)
+{
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+void printInColor(string data)
+{
+    Console.ForegroundColor = ConsoleColor.Magenta;
+    Console.Write(data);
+    Console.ResetColor();
+}
+int[,] get2DIntArray(int colLength, int rowLength, int start, int finish)
+{
+    int[,] array = new int[colLength, rowLength];
+    for (int i = 0; i < colLength; i++)
+    {
+        for (int j = 0; j < rowLength; j++)
+        {
+            array[i, j] = new Random().Next(start, finish + 1);
+        }
+    }
+    return array;
+}
+void print2DArray(int[,] array)
+{
+    Console.Write("\t");
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        printInColor(j + "\t");
+    }
+    Console.WriteLine();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        printInColor(i + "\t");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i == j)
+            {
+                printInColor(array[i, j] + "\t");
+            }
+            else
+            {
+                Console.Write(array[i, j] + "\t");
+            }
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+void findAverage(int[,] array, int n)
+{
+    Console.WriteLine("Среднее арифметическое: ");
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double average = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            average = (average + array[i, j]);
+        }
+        average = average / n;
+        Console.Write("\t");
+        Console.Write(average + " ");
+    }
+    Console.WriteLine();
+}
+int n = GetDataFromUser("Введите количество строк");
+int m = GetDataFromUser("Введите количество столбцов");
+int[,] array = get2DIntArray(n, m, 0, 10);
+print2DArray(array);
+findAverage(array, n);
